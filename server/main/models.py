530eb8883ba5dd31bd6a6670fb52b8386ba6fd40
lhk3337 <lhk3337@naver.com>
2021-05-28 16:00:45 +0900

    관리자 페이지

--- server/main/models.py
+++ server/main/models.py
@@ -11,8 +11,13 @@ class Developer(models.Model):
 class Question(models.Model):
     number=models.IntegerField(unique=True)
     content=models.CharField(max_length=50)
+    def __str__(self):
+        return f'{self.number}. {self.content}'
 
 class Choice(models.Model):
     content = models.CharField(max_length=100)
     question = models.ForeignKey(to='main.Question', on_delete=models.CASCADE)
     developer = models.ForeignKey(to='main.developer', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.content
