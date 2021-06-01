e37cd8249467fe4e77117a002480c6be180df33c
lhk3337 <lhk3337@naver.com>
2021-06-01 10:32:47 +0900

    all_results page 추가

--- server/main/urls.py
+++ server/main/urls.py
@@ -7,6 +7,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('form/', views.form, name='form'),
-    path('submit/',views.submit, name='submit'),
-    path('result/<int:developer_id>',views.result, name='result')
+    path('submit/', views.submit, name='submit'),
+    path('result/<int:developer_id>', views.result, name='result'),
+    path('all_results/', views.all_results, name='all_results')
 ]
--- server/main/views.py
+++ server/main/views.py
@@ -21,35 +21,40 @@ def form(request):
 
 
 def submit(request):
-    
+
     # 문항 수
     N = Question.objects.count()
     # 개발자 유형 수
     K = Developer.objects.count()
-    
+
     # 개발유형마다 몇 개인지 저장할 리스트 counter[1] = (1번 유형 점수(개수))
     counter = [0] * (K + 1)
-    
+
     for n in range(1, N+1):
         developer_id = int(request.POST[f'question-{n}'][0])
         counter[developer_id] += 1
-    
+
     # 최고점 개발 유형
     best_developer_id = max(range(1, K + 1), key=lambda id: counter[id])
     best_developer = Developer.objects.get(pk=best_developer_id)
     best_developer.count += 1
     best_developer.save()
-    
+
     context = {
         'developer': best_developer,
         'counter': counter
     }
-    
+
     return redirect('main:result', developer_id=best_developer_id)
+
 
 def result(request, developer_id):
     developer = Developer.objects.get(pk=developer_id)
     context = {
-        'developer' : developer,
+        'developer': developer,
     }
     return render(request, 'main/result.html', context=context)
+
+
+def all_results(request):
+    return render(request, 'main/all_results.html')
